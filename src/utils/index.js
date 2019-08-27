61fca6f62645163bd4ae0013bd5d3f0ef6f55891
pawa-wayne <wayne@pawa.co.kr>
2019-08-27 21:00:47 +0900

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,52 +1,77 @@
-// @flow
-import { OPEN } from 'ws';
-import { UNIQUE_TICKET } from '../constants';
-import type { Subscription, SubscriptionList } from '../type';
+"use strict";
 
-const heartbeat = (state: Subscription): void => {
-  Object.values(state.subscriptions).forEach((ws: any) => {
+var _interopRequireDefault = require("@babel/runtime-corejs2/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.serializeArray = exports.getEndpoint = exports.handleWsClose = exports.handleWsError = exports.handleWsOpen = void 0;
+
+var _keys = _interopRequireDefault(require("@babel/runtime-corejs2/core-js/object/keys"));
+
+var _stringify = _interopRequireDefault(require("@babel/runtime-corejs2/core-js/json/stringify"));
+
+var _entries = _interopRequireDefault(require("@babel/runtime-corejs2/core-js/object/entries"));
+
+var _values = _interopRequireDefault(require("@babel/runtime-corejs2/core-js/object/values"));
+
+var _ws = require("ws");
+
+var _constants = require("../constants");
+
+const heartbeat = state => {
+  (0, _values.default)(state.subscriptions).forEach(ws => {
     if (ws.isAlive) {
       ws.isAlive = false;
 
-      if (ws.readyState === OPEN) {
+      if (ws.readyState === _ws.OPEN) {
         ws.ping(() => {});
       }
     } else {
-      if (ws.readyState === OPEN) {
+      if (ws.readyState === _ws.OPEN) {
         ws.terminate();
       }
     }
   });
 };
 
-export const handleWsOpen = function handleWsOpen(host: string, state: Subscription, subscriptionList: SubscriptionList, openCallback: ?Function): void {
+const handleWsOpen = function handleWsOpen(host, state, subscriptionList, openCallback) {
   if (typeof openCallback === 'function') {
     openCallback(host);
   }
 
   this.isAlive = true;
-
-  const entries = Object.entries(subscriptionList);
-  const messageJson = entries.reduce((prev, [type, codes]) => ([...prev, { type, codes }]), [{ ticket: UNIQUE_TICKET }]);
-  const message = JSON.stringify(messageJson);
-
+  const entries = (0, _entries.default)(subscriptionList);
+  const messageJson = entries.reduce((prev, [type, codes]) => [...prev, {
+    type,
+    codes
+  }], [{
+    ticket: _constants.UNIQUE_TICKET
+  }]);
+  const message = (0, _stringify.default)(messageJson);
   this.send(message);
 
-  if (Object.keys(state.subscriptions).length === 0) {
-    state.intervalId = setInterval((): void => { heartbeat(state); }, 30000);
+  if ((0, _keys.default)(state.subscriptions).length === 0) {
+    state.intervalId = setInterval(() => {
+      heartbeat(state);
+    }, 30000);
   }
 
   state.subscriptions[host] = this;
 };
 
-export const handleWsError = (err: { code: string | number, message: string }): void => {
+exports.handleWsOpen = handleWsOpen;
+
+const handleWsError = err => {
   console.error(`WebSocket error : ${[err.code, err.message].filter(_ => _).join(',')}`);
 };
 
-export const handleWsClose = function handleWsClose(state: Subscription, reconnect: ?Function): void {
+exports.handleWsError = handleWsError;
+
+const handleWsClose = function handleWsClose(state, reconnect) {
   delete state.subscriptions[this.endpoint];
 
-  if (!Object.keys(state.subscriptions).length && state.intervalId) {
+  if (!(0, _keys.default)(state.subscriptions).length && state.intervalId) {
     clearInterval(state.intervalId);
   }
 
@@ -55,6 +80,12 @@ export const handleWsClose = function handleWsClose(state: Subscription, reconne
   }
 };
 
-export const getEndpoint = (host: string, pathname: string, qs?: string): string => (qs ? `${host}/${pathname}?${qs}` : `${host}/${pathname}`);
+exports.handleWsClose = handleWsClose;
 
-export const serializeArray = (arr: Array<string>, callback: Function): string => arr.map(callback).join(',');
+const getEndpoint = (host, pathname, qs) => qs ? `${host}/${pathname}?${qs}` : `${host}/${pathname}`;
+
+exports.getEndpoint = getEndpoint;
+
+const serializeArray = (arr, callback) => arr.map(callback).join(',');
+
+exports.serializeArray = serializeArray;
